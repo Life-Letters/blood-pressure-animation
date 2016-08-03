6cb5f05110aeb3b1bfd179d100cb90f04ec316bb
Torben Sko <torben.sko@gmail.com>
2016-08-03 17:34:04 +1000

    Got the heart and valve beating in time

--- app/scripts/directives/blood-pressure-animation.js
+++ app/scripts/directives/blood-pressure-animation.js
@@ -13,14 +13,63 @@ angular.module('animationBloodPressureApp')
       replace: true,
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
+      	var images = {};
+
+      	var pressureNormalMin = 80;
+      	var pressureNormalMax = 120;
+
+      	var pressureValveMin = 0;
+      	var pressureValveMax = 200;
+
+      	var heartScaleChange = 0.2;
+      	var heartbeatDuration = 2; // in seconds
+
+      	var valveSize = -1;
+      	var heartSize = -1;
 
         new p5(function(p) {
+        	var pressure = 80;
+
+        	function norm(val, min, max) {
+        		return (val-min)/(max-min);
+        	}
+
+        	p.preload = function() {
+        		images.heart 		= p.loadImage('images/icons_heart.png');
+        		images.valve 		= p.loadImage('images/icons_valve.png');
+        		images.needle 	= p.loadImage('images/icons_needle.png');
+        	};
         	p.setup = function() {
-        		console.log(element.prop('clientWidth'), element.prop('clientHeight'));
         		p.createCanvas(element.prop('clientWidth'), element.prop('clientHeight'));
+        		
+        		valveSize = p.height * 0.2;
+        		heartSize = p.height * 0.3;
         	};
         	p.draw = function() {
-        		p.ellipse(p.mouseX, p.mouseY, 10, 10);
+        		p.clear();
+
+        		// Heart beat
+        		pressure = pressureNormalMin + (p.sin(p.millis()/1000*2*p.PI/heartbeatDuration)+1)/2*(pressureNormalMax-pressureNormalMin);
+        		var valveAmount = norm(pressure, pressureValveMin, pressureValveMax);
+
+        		p.imageMode(p.CENTER);
+
+				    p.applyMatrix();
+				    p.translate(p.width/2, valveSize*1.5);
+        		p.image(images.valve, 0,0,valveSize,valveSize);
+
+        		p.angleMode(p.RADIANS);
+				    p.rotate(p.PI*valveAmount - p.PI/2);
+				    p.image(images.needle,0,0,valveSize,valveSize);
+				    p.resetMatrix();
+
+				    // Draw the heart:
+				    var heartScale = 1 + norm(pressure, pressureNormalMin, pressureNormalMax) * heartScaleChange;
+				    p.applyMatrix();
+				    p.translate(p.width-heartSize, p.height-heartSize);
+				    p.scale(heartScale)
+        		p.image(images.heart,0,0,heartSize,heartSize);
+        		p.resetMatrix();
         	};
         }, 'blood-pressure-animation');
       }
