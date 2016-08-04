6af1cb813456c3c25c31b96388c3e8d8ddf832f9
Torben Sko <torben.sko@gmail.com>
2016-08-04 12:35:20 +1000

    Added blood cells

--- app/scripts/directives/blood-pressure-animation.js
+++ app/scripts/directives/blood-pressure-animation.js
@@ -13,118 +13,179 @@ angular.module('animationBloodPressureApp')
       replace: true,
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-      	var images = {};
+      	
+      	function norm(val, min, max) {
+      		return (val-min)/(max-min);
+      	}
 
-      	var pressureNormalMin = 80;
-      	var pressureNormalMax = 120;
-      	var pressureHighMin = 100;
-      	var pressureHighMax = 180;
-
-      	var pressureValveMin = 0;
-      	var pressureValveMax = 200;
-
-      	var heartScaleChange = 0.2;
-      	var heartbeatDuration = 2; // in seconds
-
-      	var valveSize = -1;
-      	var heartSize = -1;
-
+      	var width = element.prop('clientWidth'), 
+      			height = element.prop('clientHeight');
+      	
         new P5(function(p) {
+
+        	var pressureNormalMin = 80;
+	      	var pressureNormalMax = 120;
+	      	var pressureHighMin = 100;
+	      	var pressureHighMax = 180;
+
+	      	var pressureValveMin = 0;
+	      	var pressureValveMax = 200;
+
+	      	var heartScaleChange = 0.2;
+	      	var heartbeatDuration = 2; // in seconds
+
+	    		// Artery walls:
+	    		var arteryStretchDistance = 20;
+	    		var arteryWidth = 50, 
+	    				arteryHalfWidth = arteryWidth/2;
+
+	    		var arteryMidY = height/2,
+	    				arteryTopY = arteryMidY - arteryWidth/2,
+	    				arteryBottomY = arteryMidY + arteryWidth/2;
+        		
+        	var valveSize = height * 0.2;
+        	var heartSize = height * 0.35;
+
+        	var bloodCellSpeedMin = 1;
+        	var bloodCellSpeedVariation = 4;
 
         	var pressure = 80;
         	var heartReadings = [];
+        	var bloodCells = [];
+        	var images = {};
 
-        	function norm(val, min, max) {
-        		return (val-min)/(max-min);
+        	function BloodCell(position, scale) {
+        		this.position = position;
+        		this.scale = scale;
+        		this.size = 10*scale;
+        	};
+
+        	BloodCell.prototype = {
+        		constructor: BloodCell,
+
+        		draw: function(pressure) {
+        			this.position.x += this.scale * (bloodCellSpeedMin + bloodCellSpeedVariation * (1+norm(pressure, pressureNormalMin, pressureHighMax)));
+
+        			// Have we gone too far?
+        			if ( this.position.x > width+this.size ) {
+        				this.position.x = -this.size;
+        			}
+
+        			p.fill(255, 0, 0);
+        			p.imageMode(p.CENTER);
+        			p.image(images.bloodCell, this.position.x, this.position.y, this.size, this.size);
+        		},
         	}
 
         	p.preload = function() {
-        		images.heart 		= p.loadImage('images/icons_heart.png');
-        		images.valve 		= p.loadImage('images/icons_valve.png');
-        		images.needle 	= p.loadImage('images/icons_needle.png');
+        		images.heart 			= p.loadImage('images/icons_heart.png');
+        		images.valve 			= p.loadImage('images/icons_valve.png');
+        		images.needle 		= p.loadImage('images/icons_needle.png');
+        		images.bloodCell 	= p.loadImage('images/icon_blood-cell.png');
         	};
         	p.setup = function() {
-        		p.createCanvas(element.prop('clientWidth'), element.prop('clientHeight'));
-        		
-        		valveSize = p.height * 0.2;
-        		heartSize = p.height * 0.3;
+        		p.createCanvas(width, height);
+        		p.frameRate(30);
+
+        		var bloodCellCount = 30;
+        		_.times(bloodCellCount, function(i) {
+        			bloodCells.push(
+        					new BloodCell(
+        						p.createVector(
+        							p.random(width), 
+        							p.random(arteryBottomY, arteryTopY)), 
+        						1+(i/bloodCellCount)
+        					)
+        				);
+        		});
         	};
         	p.draw = function() {
         		p.clear();
+        		p.imageMode(p.CENTER);
 
         		// Heart beat
+        		// TODO: change to something more heartbeaty
         		pressure = pressureNormalMin + (p.sin(p.millis()/1000*2*p.PI/heartbeatDuration)+1)/2*(pressureNormalMax-pressureNormalMin);
 
         		heartReadings.push(pressure);
-        		if (heartReadings.length > p.width) {
+        		if (heartReadings.length > width) {
         			heartReadings = _.tail(heartReadings);
         		}
         		_.each(heartReadings, function(r, i) {
-        			p.point(i, p.height - r);
+        			p.point(i, height - r);
         		});
 
         		var valveAmount = norm(pressure, pressureValveMin, pressureValveMax);
 
+        		// p.strokeWeight(2);
+        		// p.stroke(0);
+        		// // Gague pipe
+        		// p.line(width/2-2, valveSize, width/2-2, height/2);
+        		// p.line(width/2+2, valveSize, width/2+2, height/2);
 
-        		p.strokeWeight(2);
-        		p.stroke(0);
-        		// Gague pipe
-        		p.line(p.width/2-2, valveSize, p.width/2-2, p.height/2);
-        		p.line(p.width/2+2, valveSize, p.width/2+2, p.height/2);
 
-        		// Artery walls:
-        		var arteryStretchDistance = 20;
-        		var arteryWidth = 50, 
-        				arteryHalfWidth = arteryWidth/2;
-        		var arteryBendCtrPt = arteryStretchDistance * norm(pressure, pressureNormalMin, pressureHighMax);
-
-        		var arteryMidY = p.height/2,
-        				arteryTopY = arteryMidY - arteryWidth/2,
-        				arteryBottomY = arteryMidY + arteryWidth/2;
-
-        		// p.curve(p.width*-0.5, arteryTopY+arteryBendCtrPt, 
+        		// p.curve(width*-0.5, arteryTopY+arteryBendCtrPt, 
         		// 		0, arteryTopY, 
-        		// 		p.width, arteryTopY, 
-        		// 		p.width*1.5, arteryTopY+arteryBendCtrPt);
-        		// p.curve(p.width*-0.5, arteryBottomY-arteryBendCtrPt, 
+        		// 		width, arteryTopY, 
+        		// 		width*1.5, arteryTopY+arteryBendCtrPt);
+        		// p.curve(width*-0.5, arteryBottomY-arteryBendCtrPt, 
         		// 		0, arteryBottomY, 
-        		// 		p.width, arteryBottomY, 
-        		// 		p.width*1.5, arteryBottomY-arteryBendCtrPt);
+        		// 		width, arteryBottomY, 
+        		// 		width*1.5, arteryBottomY-arteryBendCtrPt);
+        		
+        		p.background(250,170,160);
 
-        		p.fill(250,170,160);
+        		// Blood cells:
+        		_.each(bloodCells, function(cell) {
+        			cell.draw(pressure);
+        		});
+
+        		// Artery (in reverse, i.e. we draw the surrounding whitespace)
+    				var arteryBendCtrPt = arteryStretchDistance * norm(pressure, pressureNormalMin, pressureHighMax);
+        		p.fill(255);
         		p.stroke(200,70,50);
-        		p.strokeWeight(3);
+        		p.strokeWeight(5);
 						p.beginShape();
-						p.curveVertex(p.width*-0.5, arteryMidY);
-						p.curveVertex(p.width*-0.5, arteryMidY);
+						p.curveVertex(-width, -height);
+						p.curveVertex(-width, -height);
+						p.curveVertex(-width, arteryTopY);
 						p.curveVertex(0, arteryTopY);
-						p.curveVertex(p.width*0.5, arteryTopY-arteryBendCtrPt);
-						p.curveVertex(p.width, arteryTopY);
-						p.curveVertex(p.width*1.5, arteryMidY);
-						p.curveVertex(p.width, arteryBottomY);
-						p.curveVertex(p.width*0.5, arteryBottomY+arteryBendCtrPt);
-						p.curveVertex(0, arteryBottomY);
-						p.curveVertex(p.width*-0.5, arteryMidY);
-						p.curveVertex(p.width*-0.5, arteryMidY);
+						p.curveVertex(width*0.5, arteryTopY-arteryBendCtrPt);
+						p.curveVertex(width, arteryTopY);
+						p.curveVertex(2*width, arteryTopY);
+						p.curveVertex(2*width, -height);
+						p.curveVertex(2*width, -height);
 						p.endShape();
 
-        		p.imageMode(p.CENTER);
-				    p.applyMatrix();
-				    p.translate(p.width/2, valveSize);
-        		p.image(images.valve, 0,0,valveSize,valveSize);
-
-        		p.angleMode(p.RADIANS);
-				    p.rotate(p.PI*valveAmount - p.PI/2);
-				    p.image(images.needle,0,0,valveSize,valveSize);
-				    p.resetMatrix();
+						p.beginShape();
+						p.curveVertex(2*width, height*2);
+						p.curveVertex(2*width, height*2);
+						p.curveVertex(2*width, arteryBottomY);
+						p.curveVertex(width, arteryBottomY);
+						p.curveVertex(width*0.5, arteryBottomY+arteryBendCtrPt);
+						p.curveVertex(0, arteryBottomY);
+						p.curveVertex(-width, arteryBottomY);
+						p.curveVertex(-width, height*2);
+						p.curveVertex(-width, height*2);
+						p.endShape();
 
 				    // Draw the heart:
 				    var heartScale = 1 + norm(pressure, pressureNormalMin, pressureNormalMax) * heartScaleChange;
 				    p.applyMatrix();
-				    p.translate(p.width-heartSize/2-10, p.height-heartSize/2-10);
-				    p.scale(heartScale)
+				    p.translate(width-heartSize/2-valveSize/4, height-heartSize/2-valveSize/4);
+				    p.scale(heartScale);
         		p.image(images.heart,0,0,heartSize,heartSize);
         		p.resetMatrix();
+
+        		// Valve
+        		p.applyMatrix();
+				    p.translate(width-valveSize/2-5, height-valveSize/2-5);
+        		p.image(images.valve,0,0,valveSize,valveSize);
+        		// Needle
+        		p.angleMode(p.RADIANS);
+				    p.rotate(p.PI*valveAmount - p.PI/2);
+				    p.image(images.needle,0,0,valveSize,valveSize);
+				    p.resetMatrix();
         	};
         }, 'blood-pressure-animation');
       }
